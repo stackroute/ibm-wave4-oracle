f61e260a621e2d9520683985a0aff3b895dd96da
gattinenirohini66 <gattinenirohini66>
2019-04-08 12:43:42 +0530

    register to eureka

--- bot-service/src/main/java/com/stackroute/botservice/BotServiceApplication.java
+++ bot-service/src/main/java/com/stackroute/botservice/BotServiceApplication.java
@@ -2,8 +2,10 @@ package com.stackroute.botservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BotServiceApplication {
 
 	public static void main(String[] args) {
