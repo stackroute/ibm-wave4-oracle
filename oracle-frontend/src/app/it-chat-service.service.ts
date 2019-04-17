5db788452b1c6a8960aaac7e4b70e4b75bde8128
amabath1 <amabath1@in.ibm.com>
2019-04-17 14:20:51 +0530

    sonar cloud

--- oracle-frontend/src/app/it-chat-service.service.ts
+++ oracle-frontend/src/app/it-chat-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { HttpHeaders, HttpClient } from "@angular/common/http";
 
+
 @Injectable({
   providedIn: "root"
 })
