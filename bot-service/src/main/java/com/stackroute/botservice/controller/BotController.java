d12889cb978b0d703d411bb692a84afccb724ca2
ashhwini <hashwin1@in.ibm.com>
2019-04-20 13:37:13 +0530

    socket issues

--- bot-service/src/main/java/com/stackroute/botservice/controller/BotController.java
+++ bot-service/src/main/java/com/stackroute/botservice/controller/BotController.java
@@ -18,9 +18,6 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-import java.util.Random;
-
 /* Created on : 27/03/2019 - Gopal Panchal and Subhajit Pal (@rahzex)*/
 
 @RestController
@@ -65,59 +62,6 @@ public class BotController {
     }
 
     /*
-        This method returns answer/answers for a requested question.
-    */
-
-   /* @PostMapping("/getanswer")
-    public ResponseEntity<List<SendQuery>> getAnswer(@RequestBody SendQuery sendQuery) {
-
-        String correctedQuery = restTemplate.getForObject(AUTO_CORRECTOR_URI + sendQuery.getQueryAnswer().getQuestion(), String.class).toLowerCase();
-        String concept = restTemplate.getForObject(CONCEPT_URI + correctedQuery, String.class);
-
-        logger.info(correctedQuery);
-        logger.info(concept);
-
-        List<SendQuery> response = null;
-
-        String answer = queryService.getAnswerOfSimilarQuestion(concept, correctedQuery.toLowerCase());
-        if (answer != null) {
-            response = new ArrayList<>();
-            sendQuery.setQueryAnswer(new QueryAnswer("", correctedQuery, answer));
-            sendQuery.getStatus().setAnswered(true);
-            sendQuery.getStatus().setSuggested(false);
-            response.add(sendQuery);
-        }
-        if (answer == null) {
-            response = new ArrayList<>();
-
-            Response probableAnswers = restTemplate.getForObject(NEO4J_ANSWER_URI + concept, Response.class);
-            List<QueryAnswer> queryAnswer = probableAnswers.getResponses();
-
-            for (QueryAnswer qa : queryAnswer) {
-                response.add(new SendQuery(qa, new Status(false, false,true)));
-            }
-        }
-        *//* Default answer to client if concept is not found.*//*
-        if(response.isEmpty()){
-            String[] defaultResponses = {"We don't have a answer for this question right now. We will get back to you via email.",
-                    "Oops! Seems like we have to figure it out too:( We will mail you once we figure it out:)",
-                    "Our Expert Scientist are working on this! We will get back to you via mail.",
-                    "Hmmm... looks like I don't know this yet. Let me tell this to my Human master.He will get back to you via mail."};
-
-            int replyIndex = new Random().nextInt(4);
-            response.add(new SendQuery(new QueryAnswer("","",defaultResponses[replyIndex]), new Status(false, false,false)));
-
-            // sending to manual answer service
-            QuestionDTO questionDTO = new QuestionDTO();
-            questionDTO.setConcept(concept);
-            questionDTO.setQuestion(correctedQuery);
-            kafkaTemplate.send("new_query", questionDTO);
-        }
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-*/
-    /*
         This method saves answer/answers if accepted by user else the question will be sent to
         manual answer service.
     */
@@ -153,7 +97,4 @@ public class BotController {
         return responseEntity;
     }
 }
-
-
-
 
