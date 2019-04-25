b01f7288bf1bb9e4b358513049ab3bb64d1dc9f5
gopal10 <gopalpanchal10@gmail.com>
2019-04-25 13:20:39 +0530

    little change

--- oracle-frontend/src/environments/environment.prod.ts
+++ oracle-frontend/src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const host_address="http://13.234.1.201";
 export const environment = {
   production: true,
-  saveData:`${host_address}:8081/api/v1/saveUser`,
-  loginData:`${host_address}:8081/api/v1/login`,
-  allUserData:`${host_address}:8081/api/v1/users`,
-  singleUserData:`${host_address}:8081/api/v1/user`,
-  chatHistoryService :`${host_address}:9000/api/v1`,
-  botSocket:`${host_address}:8090/socket`,
-  botService:`${host_address}/api/v1`,
+  manaul_answer:`${host_address}:8091/api/v1/`,
+  savedData:`${host_address}:8081/api/v1/saveUser`.toString(),
+  loginData:`${host_address}:8081/api/v1/login`.toString(),
+  allUserData:`${host_address}:8081/api/v1/users`.toString(),
+  singleUserData:`${host_address}:8081/api/v1/user`.toString(),
+  chatHistoryService :`${host_address}:9000/api/v1`.toString(),
+  botSocket:`${host_address}:8090/socket`.toString(),
+  botService:`${host_address}/api/v1`.toString(),
   dialogflow:{
     angularBot:'aa0e2a8f0792403c8119d2410e7aafd1'
   },
